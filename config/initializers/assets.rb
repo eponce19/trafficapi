62932ddccb9946d6d6f6b4cff2199093d72cfc01
eeveponce <eponce19@gmail.com>
2014-12-06 13:37:07 -0600

    Data base and export CSV added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,12 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( reset.css )
+Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( slider.js )
+Rails.application.config.assets.precompile += %w( superfish.js )
+Rails.application.config.assets.precompile += %w( custom.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
